e9bba06d5d362ba462c3e04f9d871d623c173d42
gorik0 <delavarus@gmail.com>
2024-07-18 14:40:26 +0300

    libs_plugins_added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+
+allprojects.forEach() {
+    proj->
+  proj.plugins.apply(libs.plugins.compose.compiler.get().pluginId)
+
 }
